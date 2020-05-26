b3e5fc824c1ef2215fc96196fe7e16f6dc1133ac
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-05-26 23:42:51 +0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.querySelector(".button").addEventListener('click', function(){
+    console.log(document.querySelector(".full-name").value);
+    console.log(document.querySelector(".email").value);
+})
